d87df5f7067458f1d878fc58ad39dcd6cbcb2f9e
qytela <julfanshahidayah@gmail.com>
2021-08-02 21:03:40 +0700

    update: favicon

--- application/views/template.php
+++ application/views/template.php
@@ -3,7 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>Modules &rsaquo; DataTables &mdash; Stisla</title>
+  <title>Spyglass K Web</title>
+
+  <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url() ?>public/assets/images/favicon.png">
 
   <!-- General CSS Files -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
